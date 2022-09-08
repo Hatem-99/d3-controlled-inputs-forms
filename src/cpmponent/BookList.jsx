8e37d6785d6c30d0f665ff18ee373dac75b0e33b
Hatem Mahmoud <hatemhindawi1999@gmail.com>
2022-09-08 15:06:22 +0200

    test

--- src/cpmponent/BookList.jsx
+++ src/cpmponent/BookList.jsx
@@ -1,22 +1,28 @@
 import { Component } from "react";
-import {Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import arrayOfBooks from "../data/history.json";
 class BookList extends Component {
-
-  
   render() {
     
     return (
       <Container>
-
-        <Row  className="justify-content-around">
-
-            {arrayOfBooks.map((book, index) => {
+        <InputGroup className="mb-3">
+            <InputGroup.Text>search</InputGroup.Text>
+          <FormControl placeholder="search for books" 
+          />
+        </InputGroup>
+        <Row className="">
+          {arrayOfBooks.map((book) => {
+            return (
+              <Col key={book.asin} className="my-3">
+              <SingleBook
                 
-              return <SingleBook key={index} objBook={{img:book.img, title:book.title}} />;
-            })}
-        
+                objBook={{ img: book.img, title: book.title }}
+              /></Col>
+    
+            );
+          })}
         </Row>
       </Container>
     );
--- src/cpmponent/SingleBook.jsx
+++ src/cpmponent/SingleBook.jsx
@@ -2,17 +2,45 @@ import { Component } from "react";
 
 import { Card } from "react-bootstrap";
 
+
+
 class SingleBook extends Component {
   state = {
     selected: false,
   };
+
+  handelClick = () => {
+    if (this.state.selected === false) {
+      this.setState({ selected: true });
+    } else {
+      this.setState({ selected: false });
+    }
+  };
+  fetchingComments =() => {
+    fetch(`https://striveschool-api.herokuapp.com/api/comments`, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzA4Y2M5Yzc1ZDZhMzAwMTU3YmE5YjYiLCJpYXQiOjE2NjI2Mzk2NjYsImV4cCI6MTY2Mzg0OTI2Nn0.ithnrGllOvKip6uRr9vtQjAzJeqsx7q05But59ZXwWI",
+      },
+    })
+    .then(data => data.json() )
+    .then(data => console.log(data))
+  };
+
+  componentDidMount = () => {
+    this.fetchingComments();
+    console.log("happend")
+  }
+
+
+
   render() {
     return (
       <Card
         className={this.state.selected ? "select" : ""}
         style={{ width: "18rem", height: "400px" }}
         onClick={() => {
-          this.setState({ selected: true });
+          this.handelClick();
         }}
       >
         <Card.Img
@@ -23,6 +51,14 @@ class SingleBook extends Component {
         />
         <Card.Body>
           <Card.Title>{this.props.objBook.title}</Card.Title>
+          {this.state.selected ? (
+            <Card.Text>
+              Comments: {} <br />
+              Rating: {}
+            </Card.Text>
+          ) : (
+            ""
+          )}
         </Card.Body>
       </Card>
     );
